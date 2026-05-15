6357ae84c601d860b9fb364da6ef938364955bcd
Jamie Lancaster <jamie.lancaster-2@student.manchester.ac.uk>
2026-05-15 16:15:49 +0100

    Packed cubie into 5-bit integers with bitwise operations

--- cubie.py
+++ cubie.py
@@ -1,31 +1,35 @@
-from dataclasses import dataclass
 
-# cubie representation
-@dataclass
-class Cubie:
-    id: int # 0-7
-    orientation: int # 0-2 1=clockwise, 2=anticlockwise
-    # orientation reference = white top, green front
 
-    # clockwise move always shifts selected 4 pieces to the right
-    # [0, 1, 2, 3] -> [3, 0, 1, 2]
+PERM_MASK = 0b111      # lower 3 bits
+ORI_MASK = 0b11000     # upper 2 bits
 
-    # 3 2 1 0
-    # 0 3 2 1
-    # 1 2 3 0
 
-    # move on U/D doesn't change orientation
-    # L/R/F'/B' [2, 1, 2, 1]
-    # F/B/L'/R' [1, 2, 1, 2]
+def get_perm(cubie):
+    return cubie & PERM_MASK
+
+
+def get_ori(cubie):
+    return cubie >> 3
+
+
+def set_perm(cubie, perm):
+    return (cubie & ORI_MASK) | (perm & 0b111)
+
+
+def set_ori(cubie, ori):
+    return (cubie & PERM_MASK) | ((ori % 3) << 3)
+
+
+def add_ori(cubie, delta):
+    ori = ((cubie >> 3) + delta) % 3
+    return (cubie & PERM_MASK) | (ori << 3)
 
 class Cube:
     def __init__(self):
-        # 8 cubies
-        # matrix to define moves
-        # func to turn into sticker view for GUI
-        # label cubie 1, 2, 3, 4... to describe permutation
-        # 0, 1, 2 to describe orientation, 0= white/yellow top, 1= clockwise, 2= anticlockwise
-        self.cubies = [Cubie(i, 0) for i in range(8)]
+        # solved state:
+        # permutation = index
+        # orientation = 0
+        self.cubies = list(range(8))
 
         self.COLOR_MAP = {
             0: "WOB",
@@ -68,93 +72,142 @@ class Cube:
         }
 
     def move_cubies(self, cubies, clockwise, orientation_value):
-        print(cubies)
-        cubies = self.orient_cubies(cubies, orientation_value, clockwise)
-        print(cubies)
-        cubies = self.perm_cubies(cubies, clockwise)
-        print(cubies)
+
+        cubies = self.orient_cubies(
+            cubies,
+            orientation_value,
+            clockwise
+        )
+
+        cubies = self.perm_cubies(
+            cubies,
+            clockwise
+        )
+
         return cubies
 
     @staticmethod
     def perm_cubies(cubies, clockwise):
+
         if clockwise:
             return cubies[-1:] + cubies[:-1]
-        else:
-            return cubies[1:] + cubies[:1]
+
+        return cubies[1:] + cubies[:1]
+
 
     @staticmethod
     def orient_cubies(cubies, change_orientation, clockwise):
+
         if change_orientation == 0:
             pattern = [0, 0, 0, 0]
+
         elif change_orientation == 1:
             pattern = [1, 2, 1, 2]
-        else:
-            pattern = [2,1,2,1]
 
-        # reverse pattern for anticlockwise
+        else:
+            pattern = [2, 1, 2, 1]
+
         if not clockwise:
             pattern = pattern[::-1]
 
-        for i in range(4):
-            cubies[i].orientation = (cubies[i].orientation + pattern[i]) % 3
+        return [
+            add_ori(cubies[i], pattern[i])
+            for i in range(4)
+        ]
 
-        return cubies
 
     @staticmethod
     def orientation_check(cubies):
-        return sum(cubie.orientation for cubie in cubies) % 3 == 0
+
+        return (
+            sum(get_ori(c) for c in cubies)
+            % 3
+            == 0
+        )
 
     def apply_move(self, move, cube_list):
-        move_indices = self.MOVES[move][0]
-        index_map = {idx: i for i, idx in enumerate(move_indices)}
 
-        selected_cubies = [cube_list[i] for i in move_indices]
-        selected_cubies = self.move_cubies(
-            selected_cubies,
+        move_indices = self.MOVES[move][0]
+
+        index_map = {
+            idx: i
+            for i, idx in enumerate(move_indices)
+        }
+
+        selected = [
+            cube_list[i]
+            for i in move_indices
+        ]
+
+        selected = self.move_cubies(
+            selected,
             self.MOVES[move][1][1],
             self.MOVES[move][1][0]
         )
 
-        temp_cubies = []
-        for i in range(len(self.cubies)):
-            if i in index_map:
-                temp_cubies.append(selected_cubies[index_map[i]])
-            else:
-                temp_cubies.append(cube_list[i])
+        temp = []
 
-        return temp_cubies
+        for i in range(8):
+
+            if i in index_map:
+                temp.append(
+                    selected[index_map[i]]
+                )
+
+            else:
+                temp.append(
+                    cube_list[i]
+                )
+
+        return temp
 
     def to_sticker_representation(self, cube_list):
+
         sticker_cube = [
-            'W', 'W', 'W', 'W',  # U 0-3
-            'Y', 'Y', 'Y', 'Y',  # D 4-7
-            'O', 'O', 'O', 'O',  # L 8-11
-            'R', 'R', 'R', 'R',  # R 12-15
-            'B', 'B', 'B', 'B',  # F 16-19
-            'G', 'G', 'G', 'G'  # B 20-23
+            'W', 'W', 'W', 'W',
+            'Y', 'Y', 'Y', 'Y',
+            'O', 'O', 'O', 'O',
+            'R', 'R', 'R', 'R',
+            'B', 'B', 'B', 'B',
+            'G', 'G', 'G', 'G'
         ]
 
-        for i in range(len(cube_list)):
+        for i in range(8):
+
             cubie = cube_list[i]
 
-            stickers = self.COLOR_MAP[cubie.id]
+            perm = get_perm(cubie)
+            ori = get_ori(cubie)
 
-            if cubie.orientation == 1:
-                # shift right
-                stickers = stickers[-1:] + stickers[:-1]
-            if cubie.orientation == 2:
-                # shift left
-                stickers = stickers[1:] + stickers[:1]
+            stickers = self.COLOR_MAP[perm]
+
+            if ori == 1:
+                stickers = (
+                        stickers[-1:]
+                        + stickers[:-1]
+                )
+
+            elif ori == 2:
+                stickers = (
+                        stickers[1:]
+                        + stickers[:1]
+                )
 
             positions = self.PERMUTATION_MAP[i]
-            for j in range(len(positions)):
-                sticker_cube[positions[j]] = stickers[j]
+
+            for j in range(3):
+                sticker_cube[
+                    positions[j]
+                ] = stickers[j]
 
         return sticker_cube
 
-
     def state_to_face(self, state):
-        state = self.to_sticker_representation(state)
+
+        state = self.to_sticker_representation(
+            state
+        )
+
         return {
             1: list(state[0:4]),
             2: list(state[8:12]),
